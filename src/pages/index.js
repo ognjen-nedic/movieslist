423b63e7b3ed3036e4ae996e0a3dc3d8986688d6
ognjen-nedic <ognjen.n94@gmail.com>
2023-05-18 18:53:49 +0200

    favicon fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -116,11 +116,11 @@ export default function Home() {
         <meta property="og:title" content="MoviesList" key="title" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/movieslist/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/movieslist/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/movieslist/favicon-16x16.png" />
+        <link rel="manifest" href="/movieslist/site.webmanifest" />
+        <link rel="mask-icon" href="/movieslist/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="apple-mobile-web-app-title" content="MoviesList" />
         <meta name="application-name" content="MoviesList" />
         <meta name="msapplication-TileColor" content="#da532c" />
